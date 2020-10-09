fc7d3d6715261ab423b21f2da39d0b4d15caae2e
chaitusolasa <72570586+chaitusolasa@users.noreply.github.com>
2020-10-09 09:50:04 +0530

    Updated security issues

--- tests/elk_functional/src/elk_functional_utilities/Kibana.java
+++ tests/elk_functional/src/elk_functional_utilities/Kibana.java
@@ -6,8 +6,8 @@ public class Kibana {
 
 	public static final String Chromedriverpath = "lib\\chromedriver.exe";
 	
+	//In case if you have used reverse proxy like nginx please use that url 
+	//public static final String Url = "http://xx.xxx.xx.xxx/app/home#/";
 	
-	public static final String Url = "http://10.177.60.134:8073/app/home#/";
-	
-	//public static final String Url = "http://locahost:5601/app/home#/";
+	public static final String Url = "http://locahost:5601/app/home#/";
 }
